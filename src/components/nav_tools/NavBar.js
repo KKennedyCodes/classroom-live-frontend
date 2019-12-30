3712cad3e40100781ccbfe42b02e786cf1e0cbfc
KKennedyCodes <holmstedtk@gmail.com>
2019-12-29 16:29:41 -0800

    Added Temporary Logo

--- src/components/nav_tools/NavBar.js
+++ src/components/nav_tools/NavBar.js
@@ -15,7 +15,7 @@ class NavBar extends React.Component {
   render () {
     return (
       <section className='navContainer'>
-        <div className="title"><h2>Classroom Live</h2></div>
+        <div className="title"><a data-flickr-embed="true" href="https://www.flickr.com/photos/186234157@N07/49296000551/in/dateposted-public/" title="ClassroomLogo"><img src="https://live.staticflickr.com/65535/49296000551_fe05df3618_b.jpg" className="title logo" alt="ClassroomLogo" /></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script></div>
         <div className="TopButton"><Button variant="outline-danger">Go Live </Button></div>
         <div className="TopButton"><Button variant="light">Logout</Button></div>
       </section>

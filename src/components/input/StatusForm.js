4865691fa91c39cb902f383769d1d97b5d2facea
KKennedyCodes <holmstedtk@gmail.com>
2020-01-08 10:10:47 -0800

    Added to Status Form

--- src/components/input/StatusForm.js
+++ src/components/input/StatusForm.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap/';
+import axios from 'axios';
 import './forms.css';
 class StatusForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      code_id: undefined,
+      comment: '',
+      publicToggle: "off",
+      public: false,
+      status: undefined,
     };
   }
 
+  onChange = (data) => {
+    this.setState({ [data.target.name]: data.target.value });
+    if (this.state.publicToggle === 'on') {
+      console.log("If statement reached");
+      this.setState({
+        public: true,
+      });}
+  }
+
+  onSubmit = (data) =>{
+    data.preventDefault();
+    axios.post('http://localhost:3000/statuses', {
+      code_id: this.state.code_id,
+      comment: this.state.comment,
+      public: this.state.public,
+      status: this.state.status,
+    }).then((response) => {
+      console.log(response);
+    }, (error) => {
+      console.log(error);
+    });
+    }
+
   render() {
     return (
-      <Form className="studentInput">
+      <Form className="studentInput" onSubmit={this.onSubmit}>
         <fieldset>
           <Form.Group>
             <Form.Label as="legend">
@@ -19,40 +48,47 @@ class StatusForm extends React.Component {
             <Form.Check
               type="radio"
               label="Stuck, Can't Make Progress"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios1"
+              name="status"
+              id="stuck"
+              value="red"
+              onChange={this.onChange}
             />
             <Form.Check
               type="radio"
               label="Question, but Still Working"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios2"
+              name="status"
+              id="question"
+              value="yellow"
+              onChange={this.onChange}
             />
             <Form.Check
               type="radio"
               label="Good, Working Fine"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios3"
+              name="status"
+              id="working"
+              value="green"
+              onChange={this.onChange}
             />
             <Form.Check
               type="radio"
               label="Done, Task Completed"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios3"
+              name="status"
+              id="done"
+              value="blue"
+              onChange={this.onChange}
             />
           </Form.Group>
         </fieldset>
           <Form.Group controlId="TaskObjective">
             <Form.Label>Comment:</Form.Label>
-            <Form.Control as="textarea" placeholer="Response Text Here" rows="3" />
+            <Form.Control as="textarea" name="comment" placeholer="Response Text Here" rows="3" onChange={this.onChange}/>
           </Form.Group>
-          {/* <Form.Group controlId="formHorizontalCheck">
-            <Form.Check label="Push to Public FAQ" />
-          </Form.Group> */}
           <Form.Check 
             type="switch"
             label="Push to Public FAQ"
             id="disabled-custom-switch"
+            onChange={this.onChange}
+            name="publicToggle"
           />
           <Button variant="light" type="submit">
             Submit
